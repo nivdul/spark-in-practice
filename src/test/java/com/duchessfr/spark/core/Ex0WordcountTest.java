127f7eb8f0d31e55d0075d495590447efcf6d1d7
Ludwine Probst <ludwine.probst@gmail.com>
2015-06-22 23:05:53 +0200

    clean TU

--- src/test/java/com/duchessfr/spark/core/Ex0WordcountTest.java
+++ src/test/java/com/duchessfr/spark/core/Ex0WordcountTest.java
@@ -6,8 +6,11 @@ import org.apache.spark.api.java.JavaRDD;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import scala.Tuple2;
 
-
+/**
+ * Here are the tests to help you to implement the Ex0Wordcount
+ */
 public class Ex0WordcountTest {
 
   private Ex0Wordcount ex0Wordcount;
@@ -33,6 +36,10 @@ public class Ex0WordcountTest {
 
     // assert
     Assert.assertEquals(381, couples.count());
+    Tuple2<String, Integer> theCouple = couples.filter(word -> "the".equals(word._1())).first();
+    Assert.assertEquals(38, theCouple._2().intValue());
+    Tuple2<String, Integer> generallyCouple = couples.filter(word -> "generally".equals(word._1())).first();
+    Assert.assertEquals(2, generallyCouple._2().intValue());
   }
 
   @Test
@@ -42,5 +49,9 @@ public class Ex0WordcountTest {
 
     // assert
     Assert.assertEquals(26, filtered.count());
+    Tuple2<String, Integer> theCouple = filtered.filter(word -> "the".equals(word._1())).first();
+    Assert.assertEquals(38, theCouple._2().intValue());
+    JavaPairRDD<String, Integer> generallyCouple = filtered.filter(word -> "generally".equals(word._1()));
+    Assert.assertEquals(0, generallyCouple.count());
   }
 }
